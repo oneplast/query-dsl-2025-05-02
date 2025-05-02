ce65a1c023695e83188b43c7e07532302638e07d
Gyeonghun Min <riverpower6@naver.com>
2025-05-02 09:29:36 +0900

    [FEAT] QueryDsl 의존성 추가

---
+++ src/main/java/org/example/querydsl20250502/user/entity/SiteUser.java
@@ -0,0 +1,30 @@
+package org.example.querydsl20250502.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
