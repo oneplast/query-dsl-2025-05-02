c0359fd4944108d2b525ca74431f7cb651ac4f1a
Gyeonghun Min <riverpower6@naver.com>
2025-05-02 09:15:56 +0900

    settings

---
+++ src/main/java/org/example/querydsl20250502/QueryDsl20250502Application.java
@@ -0,0 +1,13 @@
+package org.example.querydsl20250502;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDsl20250502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryDsl20250502Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/querydsl20250502/QueryDsl20250502ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.querydsl20250502;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDsl20250502ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
