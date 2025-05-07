06e6e6d9f64545810ca149389268890661d291df
Gyeonghun Min <riverpower6@naver.com>
2025-05-07 11:27:41 +0900

    [REFACTOR] 팔로우 추가 변경 및 가독성 향상

--- src/main/java/org/example/querydsl20250502/user/entity/SiteUser.java
+++ src/main/java/org/example/querydsl20250502/user/entity/SiteUser.java
@@ -47,7 +47,7 @@ public class SiteUser {
         interestKeywords.add(new InterestKeyword(keywordContent));
     }
 
-    public void addFollower(SiteUser follower) {
-        followers.add(follower);
+    public void follow(SiteUser u2) {
+        u2.getFollowers().add(this);
     }
 }
--- src/test/java/org/example/querydsl20250502/user/repository/UserRepositoryTest.java
+++ src/test/java/org/example/querydsl20250502/user/repository/UserRepositoryTest.java
@@ -235,7 +235,7 @@ class UserRepositoryTest {
         SiteUser u1 = userRepository.getQslUser(1L);
         SiteUser u2 = userRepository.getQslUser(2L);
 
-        u2.addFollower(u1);
+        u1.follow(u2);
 
         userRepository.save(u2);
     }
